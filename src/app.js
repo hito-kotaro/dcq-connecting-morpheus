2671873ebf95d234d31300e46f1a6499d0401a52
hito-kotaro <tohi.work@gmail.com>
2022-12-28 18:13:31 +0900

    メッセージテンプレート作成

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const { App } = require("@slack/bolt");
+const fs = require('fs');
 const axios = require("axios");
 const dotenv = require("dotenv");
+const questMsg = require('./messages/questMsg.js')
+
 dotenv.config();
 
 // 起動した時にトークンを取得して使い回すためにインスタンスのガワを作成
 let axiosInstance;
-
+const jsonObject = JSON.parse(fs.readFileSync('./src/messages/questMsg.json', 'utf8'));
 const m2mHeaders = { "content-type": "application/json" };
 const body = {
   client_id: process.env.AUTH0_CLIENT_ID,
@@ -14,6 +17,7 @@ const body = {
   grant_type: "client_credentials",
 };
 
+console.log(jsonObject)
 const fetchToken = new Promise((resolve, reject) => {
   try {
     // Auth0側でM2Mトークンの発行回数は1000/monthまでなので開発中は、同じトークンを使い回す
@@ -48,19 +52,31 @@ const app = new App({
 app.command("/quest", async ({ command, ack, say }) => {
   try {
     await ack();
-    const quests = await axiosInstance.get("quest/");
-    console.log(quests.data);
-    say("fetch quest");
+    const response = await axiosInstance.get("quest/");
+    quests = response.data.quests
+    let blocks = []
+    quests.map(async (quest) => {
+      blocks.push(questMsg.questBlock.block)
+      // quest情報を取得
+      // 置換して配列に入れる
+    })
+    console.log(blocks)
+    await say(jsonObject)
   } catch (error) {
     console.log("err");
     console.error(error);
   }
 });
 
+
+app.message('hello', async ({ message, say }) => {
+  console.log('hello')
+});
+
 //起動時の処理/////////////////////////////////////////////////////
 fetchToken.then((value) => {
   axiosInstance = axios.create({
-    baseURL: `https://${process.env.API_DOMAIN}/`,
+    baseURL: process.env.API_URL,
     headers: {
       Authorization: `Bearer ${value.data.access_token}`,
       "Content-Type": "application/json",
