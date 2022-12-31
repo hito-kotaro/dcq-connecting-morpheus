8e33ad3d7bd2e2141f105e590d6983575b32af12
hito-kotaro <tohi.work@gmail.com>
2022-12-31 15:44:32 +0900

    msg送信方法を変更

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const { App } = require("@slack/bolt");
-const fs = require('fs');
+const fs = require("fs");
 const axios = require("axios");
 const dotenv = require("dotenv");
 
@@ -7,8 +7,6 @@ dotenv.config();
 
 // 起動した時にトークンを取得して使い回すためにインスタンスのガワを作成
 let axiosInstance;
-let questMsg = JSON.parse(fs.readFileSync('./src/messages/questMsg.json', 'utf8'));
-const header = JSON.parse(fs.readFileSync('./src/messages/header.json', 'utf8'));
 
 const m2mHeaders = { "content-type": "application/json" };
 const body = {
@@ -20,19 +18,22 @@ const body = {
 
 const fetchToken = new Promise((resolve, reject) => {
   try {
+    let res;
     // Auth0側でM2Mトークンの発行回数は1000/monthまでなので開発中は、同じトークンを使い回す
-    // const res = {
-    //   data: {
-    //     access_token: process.env.AUTH0_DUMMY_TOKEN,
-    //   },
-    // };
-
-    // 本稼働中は起動ごとに毎回取得する
-    const res = axios.post(
-      `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
-      body,
-      m2mHeaders
-    );
+    if (process.env.PROD === "true") {
+      // 本稼働中は起動ごとに毎回取得する
+      res = axios.post(
+        `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+        body,
+        m2mHeaders
+      );
+    } else {
+      res = {
+        data: {
+          access_token: process.env.AUTH0_DUMMY_TOKEN,
+        },
+      };
+    }
     resolve(res);
   } catch (error) {
     console.log("err");
@@ -53,30 +54,36 @@ app.command("/quest", async ({ command, ack, say }) => {
   try {
     await ack();
     const response = await axiosInstance.get("quest/");
-    quests = response.data.quests
-    await say(header)
+    quests = response.data.quests;
+
+    await say(fs.readFileSync("./src/messages/header.json", "utf8"));
+
+    // msgBlocksのBlocksに追加していく
+    let msgBlocks = { blocks: [] };
     quests.map(async (quest) => {
-      questMsg.blocks[1].text.text = `*${quest.title}*\n*${quest.reward}* point`
-      questMsg.blocks[2].elements[0].text = quest.description
-      questMsg.blocks[3].accessory.value = String(quest.id)
-      await say(questMsg)
-    })
+      let msgTemplate = JSON.parse(
+        fs.readFileSync("./src/messages/questMsg.json", "utf8")
+      );
+      msgTemplate.blocks[0].text.text = `*${quest.title}*\n*${quest.reward}* point`;
+      msgTemplate.blocks[1].elements[0].text = quest.description;
+      msgTemplate.blocks[2].accessory.value = String(quest.id);
+      msgBlocks.blocks.push(...msgTemplate.blocks);
+    });
+    await say(msgBlocks);
   } catch (error) {
     console.log("err");
     console.error(error);
   }
 });
 
-
-app.message( async ({ message, say }) => {
-  console.log(message)
+app.message(async ({ message, say }) => {
+  console.log(message);
 });
 
-
-app.action('report', async ({ ack,say, body, client, logger }) => {
+app.action("report", async ({ ack, say, body, client, logger }) => {
   await ack();
-  say(`<@${body.user.id}> \n*let's report!!*`)
-  console.log(body)
+  say(`<@${body.user.id}> \n*let's report!!*`);
+  console.log(body);
 });
 
 //起動時の処理/////////////////////////////////////////////////////
